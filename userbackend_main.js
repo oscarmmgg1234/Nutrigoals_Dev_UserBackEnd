80dc82fd6aa0b856829c2bb1e70879478a182e0a
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2022-01-17 14:38:47 -0800

    updates

--- userbackend_main.js
+++ userbackend_main.js
@@ -14,11 +14,12 @@ const API = new http_client();
 server.listen(listening_port,null);
 
 server.get('/foodSearch', foodsearch_cache(300), function(req,res){
+        console.log("food search request with r.body: " + req.body.API_access_token);
+        console.log("food search request with r.body: " + req.body.foodID);
+        console.log("food search request with r.body: " + req.body.page_number);
+        console.log("food searh server response body: " + response);
         API.getFoodSearch(req.body.foodID, req.body.API_access_token,req.body.page_number, (response)=>{
-                console.log("food search request with r.body: " + req.body.API_access_token);
-                console.log("food search request with r.body: " + req.body.foodID);
-                console.log("food search request with r.body: " + req.body.page_number);
-                console.log("food searh server response body: " + response);
+               
                 res.send(response)
         })
       
