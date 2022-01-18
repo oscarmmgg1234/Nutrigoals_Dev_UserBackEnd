92143b4a2248454a8527b2a5b7c5da200eb52d97
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2022-01-17 21:54:31 -0800

    updates

--- userbackend_main.js
+++ userbackend_main.js
@@ -1,23 +1,21 @@
 
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser')
 const { listening_port} = require('./src/Constants/constants');
 const {http_client} = require('./src/Server/http_client')
 const foodsearch_cache = require('./src/node-cache/foodSearch-cache');
 const foodget_cache = require('./src/node-cache/foodGet-cache');
 
 const server = express();
-server.use(express.json())
+server.use(bodyParser.json())
+
 
 const API = new http_client();
 
 server.listen(listening_port,null);
 
-server.get('/foodSearch', foodsearch_cache(300), function(req,res){
-        
-        console.log("food search request with r.body: " + req.body.foodID);
-        console.log("food search request with r.body: " + req.body.page_number);
-        console.log(req.header('Content-Type'));
+server.post('/foodSearch', foodsearch_cache(300), function(req,res){
         API.getFoodSearch(req.body.foodID, req.body.API_access_token,req.body.page_number, (response)=>{
                 
                 res.send(response)
@@ -25,7 +23,7 @@ server.get('/foodSearch', foodsearch_cache(300), function(req,res){
       
 });
 
-server.get('/getFood', foodget_cache(300), function(req,res){
+server.post('/getFood', foodget_cache(300), function(req,res){
         API.getFoodWithID(req.body.foodID, req.body.API_access_token, (response)=>{
                 res.send(response)
         })
