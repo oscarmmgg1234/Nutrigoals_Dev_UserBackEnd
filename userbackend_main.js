b73813edbc2e8b22949ac33b0c535261083df234
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2022-01-17 21:15:12 -0800

    updates

--- userbackend_main.js
+++ userbackend_main.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const axios = require('axios');
-const bodyParser = require('body-parser')
 const { listening_port} = require('./src/Constants/constants');
 const {http_client} = require('./src/Server/http_client')
 const foodsearch_cache = require('./src/node-cache/foodSearch-cache');
@@ -15,10 +14,10 @@ const API = new http_client();
 server.listen(listening_port,null);
 
 server.get('/foodSearch', foodsearch_cache(300), function(req,res){
-        console.log("food search request with r.body: " + req);
+        
         console.log("food search request with r.body: " + req.body.foodID);
         console.log("food search request with r.body: " + req.body.page_number);
-        console.log(req.hostname);
+        console.log(req.header('Content-Type'));
         API.getFoodSearch(req.body.foodID, req.body.API_access_token,req.body.page_number, (response)=>{
                 
                 res.send(response)
